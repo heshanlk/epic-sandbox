5e564322f73969d79dfd4ee822595e5ef610d955
Heshan Wanigasooriya <hwanigasooriya@phase2technology.com>
2023-07-13 21:44:05 -0400

    Adding the authorization plugin to use correct set of values

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,28 +1,44 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
 
 const useSecureCookies = !!process.env.VERCEL_URL;
 
 export default NextAuth({
+  // epic-sandbox.vercel.app/api/auth/callback/epic-mychart
   providers: [
     {
       id: "epic-mychart",
       name: "Epic MyChart",
       type: "oauth",
-      wellKnown:
-        "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/.well-known/openid-configuration",
-      authorization: { params: { scope: "PATIENT.READ" } },
-      idToken: true,
-      checks: ["state"],
+      authorization: {
+        url: "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize",
+        params: { scope: "PATIENT.READ" },
+      },
+      // @ts-expect-error - TODO fix this
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          `${process.env.EPIC_MYCHART_CLIENT_ID as string}:${
+            process.env.EPIC_MYCHART_CLIENT_SECRET as string
+          }`
+        ).toString("base64")}`,
+      },
+      token: "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token",
+      userinfo: {
+        url: "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient",
+        async request({ tokens, provider }) {
+          const url = new URL(`${provider.userinfo?.url}/${tokens.patient}`);
+          return fetch(url, {
+            headers: {
+              "Content-Type": "application/fhir+json",
+              Accept: "application/json",
+              Authorization: `Bearer ${tokens.access_token}`,
+            },
+          }).then((res) => res.json());
+        },
+      },
       clientId: process.env.EPIC_MYCHART_CLIENT_ID as string,
       clientSecret: process.env.EPIC_MYCHART_CLIENT_SECRET as string,
       profile(profile) {
-        console.log(profile);
-        return {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-        };
+        return profile;
       },
     },
   ],
